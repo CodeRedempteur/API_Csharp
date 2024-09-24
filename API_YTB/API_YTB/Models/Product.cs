83c42625744e316df41defee236f7ef46f987513
CodeRedempteur <coderredemption@gmail.com>
2024-09-24 14:32:50 +0200

    Ajout de l'api local

---
+++ API_YTB/API_YTB/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿
+namespace API_YTB.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
